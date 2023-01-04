49cdf4a108b9859ea00572a7303401100adfbe2a
Lawrence Adams <papakwesiadams@icloud.com>
2023-01-04 14:27:39 -0800

    Delete 0x04-TypeScript/task_3/js directory

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++
@@ -1,7 +0,0 @@
-import { RowID, RowElement } from './interface';
-
-export function insertRow(row: RowElement): number;
-
-export function deleteRow(rowId: RowID): void;
-
-export function updateRow(rowId: RowID, row: RowElement): number;
